64aa475fc809611922e4c3fc919464d4d538c6f8
Mike <mterekho20@bw.edu>
2023-12-07 18:44:52 -0500

    day1 part1 and part2

--- day1/day1_p1.cs
+++ day1/day1_p1.cs
@@ -2,57 +2,58 @@
 using System;
 using System.Diagnostics;
 
-class Programm
+namespace AdventOfCode2023
 {
-    static void Main()
+    class Program1
     {
-        
-
-        string filePath = @"C:\Users\Michael Terekhov\advent_of_code2023\day1\inputs\day1\p1\2.txt";
-        
-        int sum = 0;
-        
-        using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        static void Main()
         {
-
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            int byteRead;
-            char first = 'd';
-            char last = 'd';
             
-            while ((byteRead = fileStream.ReadByte()) != -1)
+            string filePath = @"C:\Users\Michael Terekhov\advent_of_code2023\day1\inputs\day1\p1\2.txt";
+            
+            int sum = 0;
+            
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
-                if((char)byteRead == '\n')
+
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+
+                int byteRead;
+                char first = 'd';
+                char last = 'd';
+                
+                while ((byteRead = fileStream.ReadByte()) != -1)
                 {
-                    sum += int.Parse(first.ToString() + last.ToString());
-                    //Console.WriteLine(sum);
-                    first = 'd';
-                    last = 'd';
-                    //Console.WriteLine("-------------------------------------------------------");
-                    
-                }else{
-                    // if the first digit
-                    bool IsDigit= char.IsDigit((char)byteRead);
-
-                    if(IsDigit && first == 'd')
+                    if((char)byteRead == '\n')
                     {
-                        first = (char)byteRead;
-                        last = first; 
+                        sum += int.Parse(first.ToString() + last.ToString());
+                        //Console.WriteLine(sum);
+                        first = 'd';
+                        last = 'd';
+                        //Console.WriteLine("-------------------------------------------------------");
+                        
+                    }else{
+                        // if the first digit
+                        bool IsDigit= char.IsDigit((char)byteRead);
 
-                    }else if (IsDigit && first != 'd'){
-                        last = (char)byteRead;
+                        if(IsDigit && first == 'd')
+                        {
+                            first = (char)byteRead;
+                            last = first; 
+
+                        }else if (IsDigit && first != 'd'){
+                            last = (char)byteRead;
+                        }
                     }
-                    //Console.WriteLine("First: "+first);
-                    //Console.WriteLine("Last: "+last);
                 }
+                sum += int.Parse(first.ToString() + last.ToString());
+                Console.WriteLine(sum);
+
             }
-            sum += int.Parse(first.ToString() + last.ToString());
-            Console.WriteLine(sum);
 
+            
         }
-
-        
     }
+
 }
---
+++ day1/day1_p2.cs
@@ -0,0 +1,178 @@
+using System.IO;
+using System;
+using System.Diagnostics;
+
+namespace AdventOfCode2023
+{
+    class Program2
+    {
+        static long TicksToNanoseconds(long ticks)
+        {
+            // One tick represents 100 ns
+            const long NanosecondsPerTick = 100;
+            return ticks * NanosecondsPerTick;
+        }
+        static Dictionary<string, string> WordToDigitMapping = new Dictionary<string, string>
+        {
+            { "one", "1" },
+            { "two", "2" },
+            { "six", "6" },
+            { "four", "4" },
+            { "five", "5" },
+            { "nine", "9" },
+            { "three", "3" },
+            { "seven", "7" },
+            { "eight", "8" }
+        };
+
+        static string WordToDigit(string curr_word)
+    {
+        // Check if the word and length are present in the dictionary
+        if (WordToDigitMapping.ContainsKey(curr_word))
+        {
+            return WordToDigitMapping[curr_word];
+        }
+
+        return "";
+    }
+
+        static void Main()
+        {
+            
+            string filePath = @"C:\Users\Michael Terekhov\advent_of_code2023\day1\inputs\day1\p2\2.txt";
+            
+            int sum = 0;
+            
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+
+                int byteRead;
+                
+                string first = "";
+                string last = "";
+                
+                string curr_word = "";
+                int curr_len = 0;
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                while ((byteRead = fileStream.ReadByte()) != -1)
+                {   
+                    // if reached the end
+                    if((char)byteRead == '\n')
+                    {
+                        
+                        if(curr_word == "")
+                        {
+                            sum += int.Parse(first + last);
+                            first = "";
+                            last = "";
+                            curr_word = "";
+                            curr_len = 0;
+
+                        }
+                        else
+                        {
+                            fileStream.Seek(-curr_len, SeekOrigin.Current);
+
+                            curr_word = "";
+                            curr_len = 0;
+                            
+                        }
+
+                    }
+                    else
+                    {
+                        bool IsDigit = char.IsDigit((char)byteRead);
+                        if(IsDigit)
+                        {
+                            // if digit first in the collection a word then just use it
+                            if(curr_word == "")
+                            {
+                                if(first == "")
+                                {   
+                                    first = (char)byteRead + "";
+                                    last = first;
+                                }
+                                else
+                                {
+                                    last = (char)byteRead + "";
+                                    
+                                }
+                            }
+                            // if part of the word sequance
+                            else
+                            {
+                                
+                                curr_word += (char)byteRead + "";
+                                curr_len += 1;
+
+                                if(curr_len > 5) 
+                                {
+                                    //stop checking and move back
+                                    fileStream.Seek(-curr_len+1, SeekOrigin.Current);
+
+                                    curr_word = "";
+                                    curr_len = 0;
+                                    
+                                }
+                            }
+                        }
+                        else if(!IsDigit)
+                        {
+                            curr_word += (char)byteRead + "";
+                            curr_len += 1;   
+                            
+                            string curr_digit;
+
+                            if(curr_len >= 3 && curr_len <= 5)
+                            {
+                                curr_digit = WordToDigit(curr_word);
+
+                                if(curr_digit != "" && first == "")
+                                {
+                                    
+                                    first = curr_digit;
+                                    last = curr_digit;
+                                    // we need to go back -1 because we can have matching e or n
+                                    fileStream.Seek(-1, SeekOrigin.Current);
+                                    
+                                    curr_word = "";
+                                    curr_len = 0;
+
+
+                                }
+                                else if(curr_digit != "" && first != "")
+                                {
+                                    last = curr_digit;
+                                    // we need to go back -1 because we can have matching e or n
+                                    fileStream.Seek(-1, SeekOrigin.Current);
+                                    curr_word = "";
+                                    curr_len = 0;
+
+                                }
+                            }
+                            else if(curr_len > 5) 
+                            {
+                                //stop checking and move back
+                                fileStream.Seek(-curr_len+1, SeekOrigin.Current);
+
+                                curr_word = "";
+                                curr_len = 0;
+                            }
+
+                        }
+                    }               
+                }
+                sum += int.Parse(first + last);
+                stopwatch.Stop();
+                long elapsedTicks = stopwatch.ElapsedTicks;
+                long elapsedNanoseconds = TicksToNanoseconds(elapsedTicks);
+
+                Console.WriteLine($"Elapsed Time: {elapsedNanoseconds} nanoseconds");
+            }
+            
+            Console.WriteLine("Sum: " + sum);            
+        }
+    }
+
+}
